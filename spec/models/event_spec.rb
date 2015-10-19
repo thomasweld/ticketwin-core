2fe2eaae5937996073fa7b76ccf962708c630dc9
garettarrowood <garettarrowood@gmail.com>
2015-10-19 08:07:42 -0400

    first draft of event specs complete

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,18 +2,19 @@ require 'spec_helper'
 
 RSpec.describe Event, type: :model do
 
+  #eventually put let into spec_helper with other created models
   let(:event) { create :event }
 
   it "has a valid factory" do
     expect(event).to be_valid
   end
   
-  it "has a title that's a string" do
-    expect(event.title.class).to eq(String)
+  it "has a title" do
+    expect(event.title).to_not eq(nil)
   end
 
-  it "has a description that's text" do
-    expect(event.description.class).to eq(String)    
+  it "has a description" do
+    expect(event.description).to_not eq(nil)    
   end
 
   it "has a start_date that's a valid datetime" do
@@ -24,13 +25,37 @@ RSpec.describe Event, type: :model do
     expect(event.end_date.class).to eq(ActiveSupport::TimeWithZone)
   end
 
-  xit "has a user_id that's an integer" do
-    expect(event.user_id.class).to eq(Integer)
+  it "has a location" do
+    expect(event.location).to_not eq(nil)
+  end
+
+  it "has a start date" do
+    expect(event.start_date).to be > (Date.today)
+  end
+
+  it "has a end date later than the start date" do
+    expect(event.end_date).to be > (event.start_date)
+  end
+
+  xit "has an image" do
+    expect(event.image_file_name).to_not eq(nil)
+    #expect(event.image_content_type).to eq(%w(png jpg bmp))
+    #ask about why there are multiple image attributes
+  end
+
+  xit "has a user_id" do
+    expect(event.user_id).to_not eq(nil)
+    # but there will be a better test for this once the associations are properly set up
+  end
+
+  xit "has an oranization_id" do
+    expect(event.organization_id).to_not eq(nil)
+    # but there will be a better test for this once the associations are properly set up
   end
 
   describe "status" do
 
-    xit "is 'pending_approval' upon creation of event" do
+    it "is 'pending_approval' upon creation of event" do
       expect(event.status).to eq("pending_approval")
     end
 
@@ -43,16 +68,11 @@ RSpec.describe Event, type: :model do
       #before - set event status in proper way
       expect(event.status).to eq("after")
     end
-
   end
 
-
-  # t.string   "image_file_name"
   # t.string   "image_content_type"
   # t.integer  "image_file_size"
   # t.datetime "image_updated_at"
-  # t.string   "location"
-  # t.integer  "organization_id"
 
 end
   
