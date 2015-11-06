0556d87e768a4619858b1e8c4d38f2d89c1d7253
Kyle Cline <kyledcline@gmail.com>
2015-11-06 09:23:19 -0600

    Configure Devise

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def show
+    # authorize! :read, @user
+  end
+
+  def update
+    # authorize! :update, @user
+    respond_to do |format|
+      if @user.update(user_params)
+        sign_in(@user == current_user ? @user : current_user, :bypass => true)
+        respond_with @user, status: :ok
+      else
+        respond_with @user.errors, status: :bad_request
+      end
+    end
+  end
+
+  # GET/PATCH /users/:id/finish_signup
+  def finish_signup
+    # authorize! :update, @user
+    if request.patch? && params[:user] #&& params[:user][:email]
+      if @user.update(user_params)
+        @user.skip_reconfirmation!
+        sign_in(@user, :bypass => true)
+        respond_with @user, status: :created
+      else
+        # FIXME: What does this do?
+        @show_errors = true
+      end
+    end
+  end
+
+  # DELETE /users/:id.:format
+  def destroy
+    # authorize! :delete, @user
+    @user.destroy
+    respond_with nothing: true, status: :no_content
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    accessible = [ :name, :email ] # extend with your own params
+    accessible << [ :password, :password_confirmation ] unless params[:user][:password].blank?
+    params.require(:user).permit(accessible)
+  end
+end
