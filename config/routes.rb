3ab00c5298a652cdd99a4b7c3e8aac8fedb81064
Sylvester Willis <sylvester.lee.willis@gmail.com>
2015-11-19 22:36:14 -0500

    Added Doorkeeper for oauth capabilities.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  use_doorkeeper
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   post '/users/sign_up', :to => 'users#create'
